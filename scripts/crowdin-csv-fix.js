f8fe26325cdf591b883b49c08dd0817d0c6eea3f
Björn Büttner <Idrinth@users.noreply.github.com>
2026-01-24 22:04:56 +0100

    Update crowdin-csv-fix.js

--- scripts/crowdin-csv-fix.js
+++ scripts/crowdin-csv-fix.js
@@ -28,4 +28,4 @@ function fixCrowdinTsv(tsvContent) {
     .join('\n');
 }
 content = fixCrowdinTsv(content);
-fileName = fileName.replace(/__\.loc\.tsv/, "__");
+fileName = fileName.replace(/__(\.loc\.tsv)(.{2})/, "__$2$1");
