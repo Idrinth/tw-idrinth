8066ab0250d6a786ae66fccc7be82b2787a6d116
Björn Büttner <Idrinth@users.noreply.github.com>
2026-01-25 08:58:10 +0100

    Update post-export.crowdin.js

--- scripts/post-export.crowdin.js
+++ scripts/post-export.crowdin.js
@@ -32,9 +32,12 @@ function fixCrowdinTsv(tsvContent) {
 if (fileName.endsWith(".tsv")) {
   if (! fileName.endsWith(".loc.tsv")) {
     fileName = fileName.replace(/\.loc(\..+)\.tsv$/, "$1.loc.tsv");
+    content = fixCrowdinTsv(content)
+      .replace(/\n/, "\n#Loc;1;text/"+fileName.replace(/^.+\.loc(\..+)\.tsv$/, "$1")+"/"+fileName.replace(/\.tsv$/, "")+"\t\t\n");
+  } else {
+    content = fixCrowdinTsv(content)
+    .replace(/\n/, "\n#Loc;1;text/db/"+fileName+"\t\t\n");
   }
-  content = fixCrowdinTsv(content)
-    .replace(/\n/, "\n#Loc;1;text/"+fileName.replace(/^.+\.loc(\..+)\.tsv$/, "$1")+"/"+fileName.replace(/\.tsv$/, "")+"\t\t\n");
 } else if (fileName.endsWith(".steam")) {
-  content =  fixCrowdinTsv(content).replace(/^steam_description\t/, "").replace(/\\n/g, "\\\\n")
+  content =  fixCrowdinTsv(content).replace(/^steam_description\t/, "").replace(/\\\\n/g, "\n")
 }
