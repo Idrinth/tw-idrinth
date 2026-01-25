1caf6f26d83fb7b02d3ccf4977c5d540c2a71b77
Björn Büttner <Idrinth@users.noreply.github.com>
2026-01-25 13:58:19 +0100

    Update post-export.crowdin.js

--- scripts/post-export.crowdin.js
+++ scripts/post-export.crowdin.js
@@ -36,7 +36,7 @@ if (fileName.endsWith(".tsv")) {
     content =  content.replace(/^steam_description\t/, "").replace(/\\\\n/g, "\n")
   } else if (! fileName.endsWith(".loc.tsv")) {
     fileName = fileName.replace(/\.loc\.(.+)\.tsv$/, "$1.loc.tsv");
-    content = content.replace(/\n/, "\n#Loc;1;text/"+fileName.replace(/^.+\.(.+)\.loc\.tsv$/, "$1")+"/"+fileName.replace(/\.tsv$/, "")+"\t\t\n");
+    content = content.replace(/\n/, "\n#Loc;1;text/"+fileName.replace(/^.+__(.+)\.loc\.tsv$/, "$1")+"/"+fileName.replace(/\.tsv$/, "")+"\t\t\n");
   } else {
     content = content.replace(/\n/, "\n#Loc;1;text/db/"+fileName+"\t\t\n");
   }
